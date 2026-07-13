4185411995cbb35e47be51287d762d80d8cf0be0
rivea0 <58330360+rivea0@users.noreply.github.com>
2026-07-13 15:46:02 +0300

    Add more SourceList types, default should be wikipedia when no sourcelist is given

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,53 @@ struct Cli {
     source_list: SourceList,
 }
 
-#[derive(Args, Debug)]
+#[derive(Args, Debug, Default, PartialEq, Eq)]
 #[group(required = false, multiple = true)]
 struct SourceList {
     #[arg(long)]
     basic: bool,
+    #[arg(long, visible_alias = "zh")]
+    chinese_traditional: bool,
+    #[arg(long, visible_alias = "fr")]
+    french: bool,
+    #[arg(long, visible_alias = "de")]
+    german: bool,
+    #[arg(long, visible_alias = "hi")]
+    hindi: bool,
     #[arg(long)]
     html: bool,
     #[arg(long, visible_alias = "ja")]
     japanese_traditional: bool,
     #[arg(long)]
     le_corbusier: bool,
+    #[arg(long, visible_alias = "mlmc-zh")]
+    mlmc_chinese: bool,
+    #[arg(long, visible_alias = "mlmc-nl")]
+    mlmc_dutch: bool,
+    #[arg(long, visible_alias = "mlmc-en")]
+    mlmc_english: bool,
+    #[arg(long, visible_alias = "mlmc-fi")]
+    mlmc_finnish: bool,
+    #[arg(long, visible_alias = "mlmc-fr")]
+    mlmc_french: bool,
+    #[arg(long, visible_alias = "mlmc-de")]
+    mlmc_german: bool,
+    #[arg(long, visible_alias = "mlmc-ko")]
+    mlmc_korean: bool,
+    #[arg(long, visible_alias = "mlmc-fa")]
+    mlmc_persian: bool,
+    #[arg(long, visible_alias = "mlmc-pl")]
+    mlmc_polish: bool,
+    #[arg(long, visible_alias = "mlmc-pt")]
+    mlmc_portuguese: bool,
+    #[arg(long, visible_alias = "mlmc-ro")]
+    mlmc_romanian: bool,
+    #[arg(long, visible_alias = "mlmc-ru")]
+    mlmc_russian: bool,
+    #[arg(long, visible_alias = "mlmc-es")]
+    mlmc_spanish: bool,
+    #[arg(long, visible_alias = "mlmc-sv")]
+    mlmc_swedish: bool,
     #[arg(long)]
     nbs_iscc: bool,
     #[arg(long)]
@@ -32,7 +68,11 @@ struct SourceList {
     #[arg(long)]
     ridgway: bool,
     #[arg(long)]
-    sanzo_wada_1: bool,
+    risograph: bool,
+    #[arg(long)]
+    sanzo_wada_i: bool,
+    #[arg(long, visible_alias = "es")]
+    spanish: bool,
     #[arg(long)]
     thesaurus: bool,
     #[arg(long)]
@@ -41,27 +81,20 @@ struct SourceList {
     windows: bool,
     #[arg(long)]
     wikipedia: bool,
-    #[arg(long, visible_alias = "fr")]
-    french: bool,
-    #[arg(long, visible_alias = "es")]
-    spanish: bool,
-    #[arg(long, visible_alias = "de")]
-    german: bool,
-    #[arg(long)]
-    x11: bool,
     #[arg(long)]
     xkcd: bool,
     #[arg(long)]
-    risograph: bool,
-    #[arg(long, visible_alias = "zh")]
-    chinese_traditional: bool,
-    #[arg(long, visible_alias = "hi")]
-    hindi: bool,
+    x11: bool,
 }
 
 fn main() {
     let args = Cli::parse();
     let s = &args.pattern;
     let source = &args.source_list;
-    println!("Searching for {s} in {source:?}");
+
+    if *source == SourceList::default() {
+        println!("Searching {s} in wikipedia..."); // default
+    } else {
+        println!("Searching {s} in {:?}", source);
+    }
 }
