49a3aa408003f81edc51f07f0f28158ff0cd6048
rivea0 <58330360+rivea0@users.noreply.github.com>
2026-07-31 13:54:44 +0300

    Remove intermediary variables

--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,34 @@ use crate::helpers::{get_rgb_value, write_to_file};
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-    let pattern = &args.pattern;
-    let source_list = &args.source_list;
-    let with_info = args.with_info;
-    let output_format = &args.output;
-    let is_quiet = &args.quiet;
-    let result = ColorNameLists::search(pattern, source_list.enabled_lists(), with_info);
+    let result = ColorNameLists::search(
+        &args.pattern,
+        args.source_list.enabled_lists(),
+        args.with_info,
+    );
 
-    if let Some(colors) = result {
-        if let Some(fmt) = output_format {
-            match fmt {
+    if let Some(result) = result {
+        if let Some(output_format) = &args.output {
+            match output_format {
                 OutputFormats::Html => {
-                    write_to_file(OutputFormats::Html, &colors)?;
+                    write_to_file(OutputFormats::Html, &result)?;
                 }
                 OutputFormats::Json => {
-                    write_to_file(OutputFormats::Json, &colors)?;
+                    write_to_file(OutputFormats::Json, &result)?;
                 }
                 OutputFormats::Csv => {
-                    write_to_file(OutputFormats::Csv, &colors)?;
+                    write_to_file(OutputFormats::Csv, &result)?;
                 }
             }
         }
-        if !is_quiet {
+        if !args.quiet {
             let mut with_color = true;
             if std::env::var("NO_COLOR").is_ok() {
                 with_color = false;
             }
 
             // If truecolor is not supported, will not render as expected (https://github.com/SergioBenitez/yansi/issues/15)
-            for (list_name, values) in colors {
+            for (list_name, values) in result {
                 println!("{}", format!("\nList {:?}:", list_name));
                 if !with_color {
                     println!("{0: <30} | {1: <7}", "name", "hex");
@@ -57,7 +56,7 @@ fn main() -> Result<()> {
                 for value in values.iter() {
                     if !with_color {
                         println!("{0: <30} | {1: <7}", value.name, value.hex);
-                        if with_info {
+                        if args.with_info {
                             for (key, val) in value.meta.as_ref().unwrap() {
                                 println!("{key}: {val}\n");
                             }
@@ -70,7 +69,7 @@ fn main() -> Result<()> {
                             value.hex.rgb(rgb_vals[0], rgb_vals[1], rgb_vals[2]),
                             format!("     ").on_rgb(rgb_vals[0], rgb_vals[1], rgb_vals[2])
                         );
-                        if with_info {
+                        if args.with_info {
                             for (key, val) in value.meta.as_ref().unwrap() {
                                 println!("{key}: {val}\n");
                             }
@@ -80,7 +79,7 @@ fn main() -> Result<()> {
             }
         }
     } else {
-        println!("{pattern} is not found");
+        println!("{} is not found", &args.pattern);
     }
 
     Ok(())
