978fb3a09ab03449f3964005a1016d6b6f342ec7
rivea0 <58330360+rivea0@users.noreply.github.com>
2026-09-21 18:25:09 +0300

    Test output file contents

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,7 @@
+use anyhow::Result;
 use assert_cmd::{Command, pkg_name};
 use predicates::prelude::*;
+use tempfile::NamedTempFile;
 
 #[test]
 fn it_prints_color_from_the_default_list() {
@@ -108,4 +110,116 @@ fn it_does_not_show_output_when_the_quiet_option_is_passed() {
         .assert()
         .success()
         .stdout(predicate::str::contains("List: Wikipedia").not());
+}
+
+#[test]
+fn creates_html_file_when_passed_output_format() -> Result<()> {
+    let output_file = NamedTempFile::new()?;
+
+    let expected_text = r#"<!doctype html>
+<html lang="en">
+  <head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>colors</title>
+  </head>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    body {
+      padding: 1rem;
+    }
+    h1 {
+      text-align: center;
+    }
+    .color-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 1rem;
+      margin: 4rem 0;
+      font-family: sans-serif;
+    }
+    .color-container > div {
+      width: calc(100vw - 25%);
+      height: 8rem;
+      border-radius: 1rem;
+    }
+  </style>
+  <body>
+    <h1>List: Wikipedia</h1>
+    <div class="color-container">
+      <div style="background-color: #ff7518;"></div>
+      <p>Pumpkin</p>
+      <p>#ff7518</p>
+    </div>
+  </body>
+</html>"#;
+
+    Command::cargo_bin(pkg_name!())
+        .unwrap()
+        .arg("pumpkin")
+        .arg("--output")
+        .arg("html")
+        .arg("--file-path")
+        .arg(output_file.path())
+        .arg("--quiet")
+        .assert()
+        .success()
+        .stdout("Creating html file...\n");
+
+    let text = std::fs::read_to_string(output_file.path())?;
+    assert_eq!(text, expected_text);
+
+    Ok(())
+}
+
+#[test]
+fn creates_json_file_when_passed_output_format() -> Result<()> {
+    let output_file = NamedTempFile::new()?;
+
+    let expected_text = format!(r##"{{"Wikipedia":[{{"name":"Pumpkin","hex":"#ff7518"}}]}}"##);
+
+    Command::cargo_bin(pkg_name!())
+        .unwrap()
+        .arg("pumpkin")
+        .arg("--output")
+        .arg("json")
+        .arg("--file-path")
+        .arg(output_file.path())
+        .arg("--quiet")
+        .assert()
+        .success()
+        .stdout("Creating json file...\n");
+
+    let text = std::fs::read_to_string(output_file.path())?;
+    assert_eq!(text, expected_text);
+
+    Ok(())
+}
+
+#[test]
+fn creates_csv_file_when_passed_output_format() -> Result<()> {
+    let output_file = NamedTempFile::new()?;
+    let expected_text = "list,name,hex\nWikipedia,Pumpkin,#ff7518\n";
+
+    Command::cargo_bin(pkg_name!())
+        .unwrap()
+        .arg("pumpkin")
+        .arg("--output")
+        .arg("csv")
+        .arg("--file-path")
+        .arg(output_file.path())
+        .arg("--quiet")
+        .assert()
+        .success()
+        .stdout("Creating csv file...\n");
+
+    let text = std::fs::read_to_string(output_file.path())?;
+    assert_eq!(text, expected_text);
+
+    Ok(())
 }
