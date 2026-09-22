13dc653e5a2240869ec6859d27e0ef3c33dec572
rivea0 <58330360+rivea0@users.noreply.github.com>
2026-09-22 16:57:15 +0300

    Add metadata, format

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@
 //!
 //! `colorname` is a CLI tool to get color data based on color names in multiple languages. (Color name data collected from various sources, based on <https://github.com/meodai/color-name-lists>)
 
-
 /// Utility structs for color lists.
 pub mod models;
